3c5284a523571a975c0387a8f8a3ac870d627be3
Ammar Hussein <ammar.atef45@gmail.com>
2020-10-31 14:58:58 -0500

    added signup

--- security-service/security-service/src/main/java/edu/miu/groupx/security/securityservice/conf/AuthServerConfiguration.java
+++ security-service/security-service/src/main/java/edu/miu/groupx/security/securityservice/conf/AuthServerConfiguration.java
@@ -3,6 +3,7 @@ package edu.miu.groupx.security.securityservice.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +34,10 @@ public class AuthServerConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/home")
-                .permitAll();
+                .permitAll()
+                .antMatchers(HttpMethod.POST, "/signup")
+                .permitAll()
+                .and().csrf().disable();
         super.configure(http);
     }
 
